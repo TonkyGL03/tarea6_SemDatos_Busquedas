44aaa1b0ba17b06b71c66a40fe95a5755e154c86
Daniel Garlop <123623896+TonkyGL03@users.noreply.github.com>
2023-03-20 19:20:39 -0600

    Create Date.cpp

---
+++ Date.cpp
@@ -0,0 +1,92 @@
+#include "date.h"
+
+using namespace std;
+
+int Date::toInt() const{
+    return year * 10000 + month + 100 + day;
+}
+
+Date::Date() { }
+
+Date::Date (const Date &d) : year(d.year), month(d.month), day(d.day) { }
+
+Date& Date::operator=(const Date& d){
+    year = d.year;
+    month = d.month;
+    day = d.day;
+
+    return *this;
+}
+
+int Date::getYear() const {
+    return year;
+}
+
+int Date::getMonth() const{
+    return month;
+}
+
+int Date::getDay() const{
+    return day;
+}
+
+string Date::toString() const {
+    return to_string(day) + "/" + to_string(month) + "/" + to_string(year);
+}
+
+void Date::setYear(const int& y){
+    year = y;
+}
+
+void Date::setMonth(const int& m){
+    month = m;
+}
+
+void Date::setDay(const int& d){
+    day = d;
+}
+
+bool Date::operator==(const Date& d)const{
+    return toInt() == d.toInt();
+}
+
+bool Date::operator<=(const Date& d)const{
+    return toInt() <= d.toInt();
+}
+
+bool Date::operator>=(const Date& d)const{
+    return toInt() >= d.toInt();
+}
+
+bool Date::operator!=(const Date& d)const{
+    return toInt() != d.toInt();
+}
+
+bool Date::operator < (const Date& d)const{
+    return toInt() < d.toInt();
+}
+
+bool Date::operator > (const Date& d)const{
+    return toInt() > d.toInt();
+}
+
+ostream& operator << (ostream& os, Date& d){
+    os << d.year << endl;
+    os << d.month << endl;
+    os << d.day;
+
+    return os;
+}
+
+istream& operator >> (istream& is, Date& d){
+    string myStr;
+
+    getline(is, myStr, '/');
+    d.year = stoi(myStr);
+    getline(is, myStr, '/');
+    d.month = stoi(myStr);
+    getline(is, myStr);
+    d.day = stoi(myStr);
+
+    return is;
+}
